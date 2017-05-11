684250b073ae95f0fb8d543dd54fea0e5a263bb8
GoldenFroze <Bryllstyles@gmail.com>
2017-05-11 01:06:33 -0700

    Steam Fixer

---
+++ SteamFixer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace Steam_Fixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            string processName = "Steam";
+
+            Process[] processes = Process.GetProcessesByName(processName);
+
+
+
+            foreach (Process process in processes)
+
+            {
+
+                process.Kill();
+
+
+            }
+            {
+                string process1Name = "RzSynapse";
+                Process[] processes1 = Process.GetProcessesByName(process1Name);
+                foreach (Process process1 in processes1)
+                {
+
+                    process1.Kill();
+                }
+            }
+        }
+    }
+}
