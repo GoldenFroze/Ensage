91c774378bcfe210f143498c87ff76e8eb60161a
GoldenFroze <Bryllstyles@gmail.com>
2017-08-16 18:15:35 +0800

    Released

---
+++ StormSpiritSDK/StormSpiritSDK/Program.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="Program.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+//Credits
+//To
+//Beminee
+//Ihatevim
+
+namespace StormSharpSDK
+{
+    using System.ComponentModel.Composition;
+    using System.Windows.Input;
+
+    using Ensage;
+    using Ensage.Common.Menu;
+    using Ensage.SDK.Service;
+    using Ensage.SDK.Service.Metadata;
+
+    [ExportPlugin("StormSpiritSDK", HeroId.npc_dota_hero_storm_spirit)]
+    public class Program : Plugin
+    {
+        private readonly IServiceContext context;
+
+        [ImportingConstructor]
+        public Program([Import] IServiceContext context)
+        {
+            this.context = context;
+        }
+
+        public StormSharpConfig Config { get; private set; }
+
+        public StormSharp OrbwalkerMode { get; private set; }
+
+        protected override void OnActivate()
+        {
+            this.Config = new StormSharpConfig();
+            var key = KeyInterop.KeyFromVirtualKey((int)this.Config.Key.Value.Key);
+            this.Config.Key.Item.ValueChanged += this.HotkeyChanged;
+
+            this.OrbwalkerMode = new StormSharp(key, this.Config, this.context);
+
+
+            this.context.Orbwalker.RegisterMode(this.OrbwalkerMode);
+        }
+
+        protected override void OnDeactivate()
+        {
+            this.context.Orbwalker.UnregisterMode(this.OrbwalkerMode);
+            this.Config.Key.Item.ValueChanged -= this.HotkeyChanged;
+            this.Config.Dispose();
+        }
+
+        private void HotkeyChanged(object sender, OnValueChangeEventArgs e)
+        {
+            var keyCode = e.GetNewValue<KeyBind>().Key;
+            if (keyCode == e.GetOldValue<KeyBind>().Key)
+            {
+                return;
+            }
+
+            var key = KeyInterop.KeyFromVirtualKey((int)keyCode);
+            this.OrbwalkerMode.Key = key;
+        }
+    }
+}
