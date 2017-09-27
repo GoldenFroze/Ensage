69dd4b8b63073d55833379facc7af16e50c5f541
GoldenFroze <Bryllstyles@gmail.com>
2017-09-27 20:39:56 +0800

    Release

---
+++ Emoticons by Goldenfroze/Emoticons by Goldenfroze/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Emoticons by Goldenfroze")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f2c422b4-60e9-4908-94ec-37b768af32f0")]
