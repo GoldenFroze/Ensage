603606b1d501d105a0786f2a372871e92e794318
GoldenFroze <Bryllstyles@gmail.com>
2017-09-27 23:05:35 +0800

    GG

--- EmoticonMaster/Properties/AssemblyInfo.cs
+++ EmoticonMaster/Properties/AssemblyInfo.cs
@@ -3,6 +3,14 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyTitle("Emoticons by Goldenfroze")]
+[assembly: AssemblyTitle("EmoticonMaster")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f2c422b4-60e9-4908-94ec-37b768af32f0")]
+[assembly: AssemblyDescription("Ensage 2017")]
+[assembly: AssemblyCompany("Ensage 2017")]
+[assembly: AssemblyProduct("Ensage 2017")]
+[assembly: AssemblyCopyright("Ensage 2017")]
+[assembly: AssemblyTrademark("Ensage 2017")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
