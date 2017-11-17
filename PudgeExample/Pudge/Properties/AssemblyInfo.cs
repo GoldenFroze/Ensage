8a2fd281f079503869f7bc595dd89c5b3d7f42d3
GoldenFroze <Bryllstyles@gmail.com>
2017-11-17 22:01:56 +0800

    Deleted

--- PudgeExample/Pudge/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-
-[assembly: AssemblyTitle("Pudge")]
-[assembly: ComVisible(false)]
-[assembly: Guid("175d73b8-aec8-4624-8982-de656c5c305e")]
